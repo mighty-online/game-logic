ec684341870230cae901ba07f9dd5659ccff6887
SyphonArch <jake.hyun@hotmail.com>
2019-06-16 05:11:01 +0900

    Implemented trick_winner function

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-"""The game logic module built for Tranquil Tempest (and perhaps also the backend engine?)"""
+"""The game logic module of Mighty, built for the Mighty-Online project."""
 
 __author__ = "Jake Hyun (SyphonArch)"
 __copyright__ = "Copyright 2019, The Mighty-Online Team"
@@ -9,8 +9,68 @@ __maintainer__ = "Jake Hyun (SyphonArch)"
 __email__ = "jake.hyun@hotmail.com"
 __status__ = "Development"
 
-cards = ['SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
-         'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-         'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-         'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
-         'JK']
+suits = ['S', 'D', 'H', 'C']
+suits_short_to_long = {'S': 'Spades', 'D': 'Diamonds', 'H': 'Hearts', 'C': 'Clubs'}
+pointcard_ranks = ['X', 'J', 'Q', 'K', 'A']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9'] + pointcard_ranks
+
+# Note that 10 is represented by X in order to keep all card name lengths consistent
+joker = 'JK'
+joker_call = 'JC'  # the Joker Call card should be substituted by JC if it is played as a Joker Call.
+cards = ['SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'SX', 'SJ', 'SQ', 'SK',
+         'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DX', 'DJ', 'DQ', 'DK',
+         'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'HX', 'HJ', 'HQ', 'HK',
+         'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CX', 'CJ', 'CQ', 'CK'] + [joker]
+
+
+# Player number is a value in range(5)
+def next_player(prev_player: int) -> int:
+    """Returns the number of the next player, given the previous player's number.
+    """
+    return (prev_player + 1) % 5
+
+
+def trick_winner(trick: list, trump: str) -> int:
+    """Returns the winner of the trick, given the trick and the trump suit
+    """
+    try:
+        assert len(trick[0]) == 2
+        assert type(trick[0][0]) == int
+        assert type(trick[0][1]) == str
+    except (AssertionError, TypeError):
+        raise AssertionError('Tricks should be in the format of [(player_num, played_card),...]')
+
+    # Setting the Mighty card
+    if trump == 'S':
+        mighty = 'DA'
+    else:
+        mighty = 'SA'
+
+    # Searching for Mighty
+    for player, card in trick:
+        if card == mighty:
+            return player
+
+    # Searching for Joker
+    if not trick[0][1] == joker_call:  # if Joker Call is not led
+        for player, card in trick:
+            if card == joker:
+                return player
+
+    suit_led = trick[0][1][0]
+    for target_suit in (trump, suit_led):  # Searches for trumps, then plays which match the suit led
+        target_plays = [play for play in trick if play[1][0] == target_suit]
+        if target_plays:
+            target_plays.sort(key=lambda p: ranks.index(p[1][1]), reverse=True)
+            return target_plays[0][0]
+
+    raise RuntimeError('No winning card found in trick')
+
+
+def is_pointcard(card: str) -> bool:
+    """Returns whether the given card is a point card
+    """
+    if card == joker:
+        return False
+    else:  # If not a Joker, all cards ending with an alphabet are point cards. (Because 10 is also X)
+        return card[1].isalpha()
